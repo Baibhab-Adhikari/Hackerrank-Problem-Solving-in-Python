77f9f5a6864f464a04e3e1e755e5cdaf4deebedd
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-08-18 18:28:05 +0530

    Added solution for A Very Big Sum

---
+++ A-very-big-sum/solution.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar:list[int]) -> int:
+    # Write your code here
+    summation: int = 0  # declare summation to store the total
+    for i in ar:
+        summation += i
+    return summation
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
